b855859b8659bef933d0e5bb382fd380299d4a13
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-03-07 10:47:30 +0300

    feat: add value scaling by size difference for blur filter

--- script.js
+++ script.js
@@ -114,9 +114,14 @@ function drawImage() {
       canvas.width = img.width;
       canvas.height = img.height;
 
+      const sizeScaling = 5 * ((img.width / image.width + img.height / image.height) / 2);
+
       const ctx = canvas.getContext('2d');
       let filters = '';
       filtersContainer.querySelectorAll('input').forEach((input) => {
+        if (input.name === 'blur')
+          filters += `${input.name}(${sizeScaling}${input.dataset.sizing}) `;
+
         filters += `${input.name}(${input.value}${input.dataset.sizing}) `;
       });
       ctx.filter = filters.trim();
